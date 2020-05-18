fd74f749d41024f8d51376d5c068e5a71d6ca438
julia-wrobel <jw3134@cumc.columbia.edu>
2020-05-18 14:12:41 -0600

    changes commit

--- R/registr.R
+++ R/registr.R
@@ -125,15 +125,13 @@ registr = function(obj = NULL, Y = NULL, Kt = 8, Kh = 4, family = "binomial", gr
     if (gradient) {gradf = loss_h_gradient} else {gradf = NULL}
     
     if(parametric_warps == "monotone_prior"){
-    	beta_optim = optim(beta_i, loss_h, method = "BFGS",
+    	beta_optim = optim(beta_i, loss_h,
     										 gr = NULL, Y = Yi,
     										 Theta_h = Theta_h_i, mean_coefs = mean_coefs_i,
     										 knots = global_knots, family = family,
     										 t_min = t_min, t_max = t_max,
     										 parametric_warps = parametric_warps, ...)
-    	
-    	
-    	
+
     }else{
     	beta_optim = constrOptim(beta_i, loss_h, grad = gradf, ui = ui, ci = ci, 
     													 Y = Yi, 
