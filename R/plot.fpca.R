c2ee377f586df3e05f0aa994521dd221be62c2f9
bauer-alex <alexander.bauer@stat.uni-muenchen.de>
2020-12-02 10:56:50 +0100

    minor fixes of plot.fpca

--- R/plot.fpca.R
+++ R/plot.fpca.R
@@ -6,27 +6,29 @@
 #' the global mean. based on an object created with function
 #' \code{\link{fpca_gauss}}, \code{\link{bfpca}} or \code{\link{gfpca_twoStep}}. \cr \cr
 #' The shares of explained variance are included in the plot titles if
-#' \code{fpca_obj} contains an element \code{evalues_sum}. This is currently
+#' \code{x} contains an element \code{evalues_sum}. This is currently
 #' only the case for FPCA objects created with \code{\link{gfpca_twoStep}}.
 #' 
-#' @param fpca_obj Object of class \code{"fpca"}.
+#' @param x Object of class \code{"fpca"}.
 #' @param plot_npc Optional number of the main FPCs to be plotted. Defaults to
-#' all FPCs contained in \code{fpca_obj}.
+#' all FPCs contained in \code{x}.
 #' @param var_factor Numeric factor with which the FPC's are multiplied
 #' to display their variation in the plots. Defaults to 2, but can be set to
 #' 2 times the standard deviation of the obtained FPC scores.
 #' @param response_function Optional response function to be applied before
 #' plotting the curves. Defaults to \code{NULL}, i.e. the identity function if
-#' \code{fpca_obj$family} is one of \code{c("gaussian","binomial")} or
-#' \code{exp()} if \code{fpca_obj$family = "gamma"}.
+#' \code{x$family} is one of \code{c("gaussian","binomial")} or
+#' \code{exp()} if \code{x$family = "gamma"}.
 #' @param subtitle If TRUE (default) the parameter \code{var_factor}
 #' is displayed in the plot subtitle.
 #' @param xlim,ylim Optional numeric vectors with limits for the x and y axis.
 #' @param xlab,ylab Optional titles for the x and y axis.
+#' @param ... Additional arguments passed to \code{\link[ggplot2]{theme}}.
 #' 
 #' @import ggplot2
 #' @importFrom cowplot plot_grid
 #' @importFrom dplyr bind_rows
+#' @export
 #' 
 #' @return Grid of \code{ggplot} plots, created with \code{cowplot::plot_grid}.
 #' 
@@ -38,16 +40,16 @@
 #' fpca_obj = fpca_gauss(Y = growth_incomplete, npc = 2)
 #' plot(fpca_obj)
 #' 
-plot.fpca = function(fpca_obj, plot_npc = fpca_obj$npc, var_factor = 2,
+plot.fpca = function(x, plot_npc = x$npc, var_factor = 2,
                      response_function = NULL,
                      subtitle = TRUE, xlim = NULL, ylim = NULL,
-                     xlab = "t [registered]", ylab = "y") {
+                     xlab = "t [registered]", ylab = "y", ...) {
   
   # some NULL variable definitions to appease CRAN package checks regarding the use of ggplot2
   fpc_value <- type <- value <- NULL
   
   if (is.null(response_function)) {
-    if (fpca_obj$family == "gamma") {
+    if (x$family == "gamma") {
       response_function = exp
     } else {
       response_function = function(x) { x }
@@ -55,19 +57,19 @@ plot.fpca = function(fpca_obj, plot_npc = fpca_obj$npc, var_factor = 2,
   }
   
   # data preparation
-  mean_dat = data.frame(t    = fpca_obj$t_vec,
-                        mean = as.vector(fpca_obj$mu))
-  fpc_dat  = data.frame(id        = rep(1:fpca_obj$npc, each = length(fpca_obj$t_vec)),
-                        t         = rep(fpca_obj$t_vec, times = fpca_obj$npc),
-                        fpc_value = as.vector(fpca_obj$efunctions))
+  mean_dat = data.frame(t    = x$t_vec,
+                        mean = as.vector(x$mu))
+  fpc_dat  = data.frame(id        = rep(1:x$npc, each = length(x$t_vec)),
+                        t         = rep(x$t_vec, times = x$npc),
+                        fpc_value = as.vector(x$efunctions))
   
   # apply response function to mean
   mean_dat = mean_dat %>%
     mutate(mean_y = response_function(mean))
   
   # prepare eigenvalue information for the plot titles
-  if (!is.null(fpca_obj$evalues_sum)) {
-    ev_share = round(100 * fpca_obj$evalues / fpca_obj$evalues_sum, 1)
+  if (!is.null(x$evalues_sum)) {
+    ev_share = round(100 * x$evalues / x$evalues_sum, 1)
     ev_share[ev_share == 0] = "<1"
     ev_info  = paste0(" (",ev_share,"%)")
   }
@@ -120,11 +122,12 @@ plot.fpca = function(fpca_obj, plot_npc = fpca_obj$npc, var_factor = 2,
       geom_text(data = symbolPlus_dat,  label = "+", size = 7, col = "gray40") +
       geom_text(data = symbolMinus_dat, label = "-", size = 7, col = "gray40") +
       xlim(xlim) + ylim(ylim) + xlab(xlab) + ylab(ylab) +
-      ggtitle(paste0("FPC ", i, ifelse(!is.null(fpca_obj$evalues_sum), ev_info[i], ""),
+      ggtitle(paste0("FPC ", i, ifelse(!is.null(x$evalues_sum), ev_info[i], ""),
                      ifelse(subtitle, paste0("\n(mean +/- ",var_factor,"*FPC)"), ""))) +
       theme(legend.position  = "none",
             panel.grid.minor = element_blank(),
-            plot.title       = element_text(hjust = 0.5))
+            plot.title       = element_text(hjust = 0.5),
+            ...)
   })
   
   # plot grid
