be12b792dc0655a325f0729708de126ad4f684b2
bauer-alex <alexander.bauer@stat.uni-muenchen.de>
2020-12-03 19:48:47 +0100

    #26 unit tests

--- tests/testthat/test-register_fpca.R
+++ tests/testthat/test-register_fpca.R
@@ -218,9 +218,9 @@ test_that("register_fpca only accepts Y_template if it has the correct format.",
 	
 	template_ids1 = "girl18"
 	Y_template1   = Y[Y$id %in% template_ids1,]
-	template_ids2 = "boy30"
+	template_ids2 = "girl36"
 	Y_template2   = Y[Y$id %in% template_ids2,]
-	template_ids3 = c("boy01","boy29","boy30","boy31","boy34","boy36")
+	template_ids3 = c("boy01","boy04","boy29","boy30","boy31","boy34","boy36")
 	Y_template3   = Y[Y$id %in% template_ids3,]
 	
 	expect_error(register_fpca(Y = Y, family = "gaussian", Y_template = Y_template1$value),
