d40bdc24caaf873f4150495f1c5152ad370bebd1
julia-wrobel <julia.wrobel@cuanschutz.edu>
2025-10-15 11:50:29 -0400

    update makefile and other documentation

--- R/help.R
+++ R/help.R
@@ -2,10 +2,8 @@
 #'
 #' Software for registering functional data from the exponential family of distributions.
 #' 
-#' @name registr
 #' @author Julia Wrobel
-#' @docType package
 #'
 #' @useDynLib registr
 #' @importFrom Rcpp evalCpp
-NULL
+"_PACKAGE"
